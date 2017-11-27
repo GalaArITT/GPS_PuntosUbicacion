26f2994e4f2af5066dc981f85bd2fb4893dfccf9
GalaArITT <oljagalavizar@ittepic.edu.mx>
2017-11-27 01:25:35 -0700

    imagen legendaria.

--- app/src/main/java/com/example/oliver/gps_puntosubicacion/MapsActivity.java
+++ app/src/main/java/com/example/oliver/gps_puntosubicacion/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.oliver.gps_puntosubicacion;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -18,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -50,16 +52,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -127,7 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (title.length() > 2) {
             MarkerOptions markerOpt1 = new MarkerOptions()
-                    .title(title)
+                    .title(title).icon(BitmapDescriptorFactory.fromResource(R.drawable.sama))
                     .anchor(0.5f, 0.5f);
             markerOpt1.position(latLng);
             mMap.addMarker(markerOpt1);
